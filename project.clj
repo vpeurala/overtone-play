23a71cdd7608924022667d8aa7be6a5d2290ad98
Ville Peurala <ville.peurala@gmail.com>
2018-09-20 17:10:27 +0300

    Add SuperCollider libs

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [overtone "0.10.3"]]
-  :main ^:skip-aot hertz.core
+  :main ^:skip-aot overtone-play.core
   :native-path "native"
   :target-path "target/%s"
   :jvm-opts ^:replace []
--- src/overtone_play/core.clj
+++ src/overtone_play/core.clj
@@ -1,7 +1,10 @@
 (ns overtone-play.core
+  (:require [overtone.live :as ol]
+            [overtone.inst.sampled-piano :as oisp])
+  (:use overtone.core)
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (oisp/sampled-piano 60))
